1d633403b0c5262faf5a5157811e23d146addd0a
leyarotheconquerer <HazenJohnson@letu.edu>
2014-08-22 22:05:15 -0500

    Add initial working deployment of Phaser.

---
+++ resources/scripts/main.js
@@ -0,0 +1,19 @@
+// Main.js
+
+var Game = new Phaser.Game(
+		800,600,
+		Phaser.Canvas, "game",
+		{
+			preload: preload,
+			create: create,
+			update: update
+		});
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
